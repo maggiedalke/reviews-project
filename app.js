fceb8f776485ad2d8edfe469d36611c328d8612a
Maggie Dalke <cdalke4@msn.com>
2020-12-29 18:39:45 -0600

    Adding functionality to buttons

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const reviews = [
   },
 ];
 
-// select items
+// Select DOM elements
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
@@ -51,12 +51,44 @@ const randomBtn = document.querySelector('.random-btn');
 // set starting item
 let currentItem = 0;
 
-// load intial item
+// Functions
 
-window.addEventListener('DOMContentLoaded', function () {
+// show person based on item
+function showPerson() {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+// Event Listeners
+
+// load intial item
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentItem);
+});
+
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// show random person
+randomBtn.addEventListener('click', function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson();
 });
